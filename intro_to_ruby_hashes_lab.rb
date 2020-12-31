745d09efa59d814902afb37390b3ed8360884358
sydney miranda <sydmiranda@icloud.com>
2020-12-31 21:28:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,6 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
   railroads = {}
-
 end
 
 def monopoly_with_second_tier
